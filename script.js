799ef40fe5d05d5a7b5f18d485475fe63dcec13b
mshakhriyorov <mshakhriyorov8@gmail.com>
2020-11-19 19:15:18 +0500

    second commit

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
+  
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -52,6 +53,7 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt('J').toLowerCase('j');
+
 
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
